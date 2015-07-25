a3ff37eda0c15fc93c1b08cad8ce6f1e66c14861
dkoops <david.koops@gmail.com>
2015-07-25 11:04:35 +1000

    Data Science - Programming Assignment2 - Complete

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This group of functions is used to create, store and retrieve
+## a precalculated inverse matrix to save time in future computions
 
-## Write a short comment describing this function
+## makeCacheMatrix function creates a list of four functions 
+##                 to manage the setting and retreiving of the
+##                 matrix and inverse matrix objects
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  cxinv <- NULL
+  set <- function(y) {
+    x <<- y
+    cxinv <<- NULL
+  }
+  get <- function() x
+  setxinverse <- function(xinv) cxinv <<- xinv
+  getxinverse <- function() cxinv
+  list(set = set, get = get,
+       setxinverse = setxinverse,
+       getxinverse = getxinverse)
 }
 
 
-## Write a short comment describing this function
+## casheSolve function is used to retrieve the cached Matrix inverse 
+##            or calculate the inverse and set it, if it does not exist
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  xinv <- x$getxinverse()
+  if(!is.null(xinv)) {
+    message("getting cached data")
+    return(xinv)
+  }
+  data <- x$get()
+  xinv <- solve(data)
+  x$setxinverse(xinv)
+  xinv
 }
